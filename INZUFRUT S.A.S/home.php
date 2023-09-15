037d35cb021fd4e6fc8ffa6a6da2e31b9b9f4b1a
gionorte <94419522+gionorte@users.noreply.github.com>
2023-09-14 20:47:16 -0500

    Update home.php - add table

--- INZUFRUT S.A.S/home.php
+++ INZUFRUT S.A.S/home.php
@@ -12,6 +12,8 @@
 	<link rel="stylesheet" href="css/material-design-iconic-font.min.css">
 	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />  
+	<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
 	<script src="js/material.min.js" ></script>
