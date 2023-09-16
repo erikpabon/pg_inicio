0e3e4d3b50346f7a5c99f9ce369a30bc53cb17d2
tovar <tovarpabon2301@gmail.com>
2023-09-15 19:15:12 -0500

    subir

---
+++ INZUFRUT S.A.S/gerente/conexion.php
@@ -0,0 +1,18 @@
+
+<?php 
+
+
+$conexion = mysqli_connect("localhost","root","","proyecto_inzuf");
+
+/*if ($conexion){
+    echo 'Conectado exitosamente a la DB';
+}
+
+else{
+    echo 'oops algo salio mal';
+
+}*/
+
+
+
+?>
